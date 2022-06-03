e95ba48f4882732f78c0aaed05583bf281ef04df
Jake Felice <jasonfelice0@gmail.com>
2022-06-03 02:48:44 -0700

    Create a categories page

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {
 } from 'react-router-dom';
 import Books from './pages/Books';
 import NoPage from './pages/NoPage';
+import Categories from './pages/Categories';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Books />} />
       <Route path="*" element={<NoPage />} />
+      <Route path="/categories" element={<Categories />} />
     </Routes>
   </Router>
 );
