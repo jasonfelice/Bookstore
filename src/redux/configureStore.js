4dd935f23da908eba2f577c3954d10b6fcf01153
Jake Felice <jasonfelice0@gmail.com>
2022-06-06 05:29:53 -0700

    Create a store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,3 +1,4 @@
+import { createStore } from 'react-redux';
 import { combineReducers } from 'redux';
 import addRemove from './books/books';
 import categories from './categories/categories';
@@ -6,3 +7,7 @@ const rootReducer = combineReducers({
   addRemove,
   categories,
 });
+
+const store = createStore(rootReducer);
+
+export default store;
