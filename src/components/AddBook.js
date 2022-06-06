867d716bfbfb79bccd2ebfffb0d76d3301140e5d
Jake Felice <jasonfelice0@gmail.com>
2022-06-06 08:53:31 -0700

    Implement dispatch function in the AddBook component

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const Book = () => (
-  <div className="add_book">
-    <h3>Add New Book</h3>
-    <form action="#">
-      <input type="text" name="title" />
-      <input type="text" name="author" />
-      <input type="submit" value="Add Book" />
-    </form>
-  </div>
-);
+const Book = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="add_book">
+      <h3>Add New Book</h3>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(addBook());
+        }}
+        action="#"
+      >
+        <input type="text" name="title" />
+        <input type="text" name="author" />
+        <input type="submit" value="Add Book" />
+      </form>
+    </div>
+  );
+};
 
 export default Book;
