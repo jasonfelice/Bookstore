3840ad359db1b620c1938e314a2c45b4ecced2c2
Jake Felice <jasonfelice0@gmail.com>
2022-06-08 07:19:38 -0700

    Install sass loader

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './Header.module.css';
+import styles from './Header.module.scss';
 
 const Header = () => {
   const links = [
